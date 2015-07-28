7a7b765e1fdd4570ab48da5784134d0fc46d3835
Aditi Jain <aj323@cornell.edu>
2015-07-27 20:53:48 -0500

    code snippets with spotipy library

--- MusicMixer.py
+++ MusicMixer.py
@@ -1,1 +1,26 @@
 print "hello"
+
+import spotipy
+
+birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+spotify = spotipy.Spotify()
+
+results = spotify.artist_albums(birdy_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
+
+lz_uri = 'spotify:artist:36QJpDe2go2KgaRleHCDTp'
+
+spotify = spotipy.Spotify()
+results = spotify.artist_top_tracks(lz_uri)
+
+for track in results['tracks'][:10]:
+    print 'track    : ' + track['name']
+    print 'audio    : ' + track['preview_url']
+    print 'cover art: ' + track['album']['images'][0]['url']
+    print
